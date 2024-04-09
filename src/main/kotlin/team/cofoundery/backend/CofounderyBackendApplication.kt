dba9d852e1fc1d2ad6a2c7286d6695893fd16bee
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-04-09 14:22:23 +0900

    Initialize project

---
+++ src/main/kotlin/team/cofoundery/backend/CofounderyBackendApplication.kt
@@ -0,0 +1,11 @@
+package team.cofoundery.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CofounderyBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CofounderyBackendApplication>(*args)
+}
---
+++ src/test/kotlin/team/cofoundery/backend/CofounderyBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.cofoundery.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CofounderyBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
